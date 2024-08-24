e3524daa704953b5e1d789e1ecb1acbdc07993fa
Craig Thacker <craigthackerx@gmail.com>
2024-08-24 01:29:34 +0100

    update hardening kitty

--- packer/windows/windows-server2022-azure/scripts/HardeningKitty/Invoke-HardeningKitty.ps1
+++ packer/windows/windows-server2022-azure/scripts/HardeningKitty/Invoke-HardeningKitty.ps1
@@ -1,1 +1,1 @@
-Invoke-HardeningKitty -Mode HailMary -Log -Report -FileFindingList .\lists\$Env:HARDENING_KITTY_FILES_TO_RUN
+Invoke-HardeningKitty -Mode HailMary -Log -Report -FileFindingList $Env:HARDENING_KITTY_PATH\lists\$Env:HARDENING_KITTY_FILES_TO_RUN
