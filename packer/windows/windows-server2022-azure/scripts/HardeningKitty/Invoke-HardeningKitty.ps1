d0706446526db2eb677aec4960b76d6878652549
Craig Thacker <craigthackerx@gmail.com>
2024-08-24 01:42:31 +0100

    update child item

--- packer/windows/windows-server2022-azure/scripts/HardeningKitty/Invoke-HardeningKitty.ps1
+++ packer/windows/windows-server2022-azure/scripts/HardeningKitty/Invoke-HardeningKitty.ps1
@@ -1,1 +1,4 @@
-Invoke-HardeningKitty -Mode HailMary -Log -Report -FileFindingList $Env:HARDENING_KITTY_PATH\lists\$Env:HARDENING_KITTY_FILES_TO_RUN
+Set-Location -Path $Env:HARDENING_KITTY_PATH
+Get-ChildItem
+
+Invoke-HardeningKitty -Mode HailMary -Log -Report -FileFindingList .\lists\$Env:HARDENING_KITTY_FILES_TO_RUN
