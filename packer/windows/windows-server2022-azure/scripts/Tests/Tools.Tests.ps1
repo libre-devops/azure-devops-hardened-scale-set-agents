bbe082298420116f6a83524e5aca54d0e5d2b896
Craig Thacker <craigthackerx@gmail.com>
2024-09-14 02:42:32 +0100

    update

--- packer/windows/windows-server2022-azure/scripts/Tests/Tools.Tests.ps1
+++ packer/windows/windows-server2022-azure/scripts/Tests/Tools.Tests.ps1
@@ -8,24 +8,8 @@ Describe "PowerShell Core" {
     }
 }
 
-
 Describe "Pipx" {
     It "Pipx" {
         "pipx --version" | Should -ReturnZeroExitCode
-    }
-}
-
-Describe "OpenSSL" {
-    It "OpenSSL Version" {
-        $OpenSSLVersion = (Get-ToolsetContent).openssl.version
-        openssl version | Should -BeLike "* ${OpenSSLVersion}*"
-    }
-
-    It "OpenSSL Path" {
-        (Get-Command openssl).Source -eq (Join-Path ${env:ProgramFiles} 'OpenSSL\bin\openssl.exe') | Should -Be $true
-    }
-
-    It "OpenSSL Full package" {
-        Join-Path ${env:ProgramFiles} 'OpenSSL\include' | Should -Exist
     }
 }
